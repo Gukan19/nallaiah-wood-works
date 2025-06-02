c21464532dfd1318a008d69553ff58f80f06044b
Gukan N <gukanguna615@gmail.com>
2025-06-02 21:44:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,8 @@ function App() {
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3915.889941221593!2d78.33026881526042!3d10.066548992842876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3babf7caac0f3cc3%3A0xdbe2fa185ea60129!2sSree%20Mariyamman%20Nagar%2C%20Karaikudi!5e0!3m2!1sen!2sin!4v1680000000000!5m2!1sen!2sin";
   
   const mapsLink =
-    "https://maps.app.goo.gl/nGw2B8tqA6snRt5y8?g_st=aw";
+    "https://www.google.com/maps?q=10.0672560,78.8133250"
+;
 
   const phoneNumber = "9442919375";
 
